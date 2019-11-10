2d3359c4f8c4ab0fbdee60f23cfee356bd678fd1
thelittlestschnauzer <doneil1580@gmail.com>
2019-11-10 18:16:46 -0500

    adding username to users and admins with option to login with either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "only letters and numbers for this one please"}
+  attr_accessor :login 
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup 
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first 
+    end  
+  end
 end
